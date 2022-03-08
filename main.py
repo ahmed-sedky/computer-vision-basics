ec035c295375e9ef26c6ff0764a9a7eebadd57ae
Sedky99 <hossedky@gmail.com>
2022-03-08 02:59:48 +0200

    normalize updates & utilities

--- main.py
+++ main.py
@@ -3,20 +3,17 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 # Load an color image 
-img = cv2.imread('images/apple.jpg') 
+img = cv2.imread('images/lenna.png') 
 #========================================================
 
-# show original image
-
-# cv2.imshow('image',img) # show original image
-# cv2.waitKey(0)
-#=========================================================
-
-#call normalization
-new_max = 255 
-new_min = 100
-normalization.normalize(img ,new_max ,new_min)
-#=========================================================
+#call filters
+# img = cv2.cvtColor(img , cv2.COLOR_BGR2GRAY)
+# filters.average_filter(img, 3)
+# filters.median_filter(img)
+# filters.gaussian_filter(img,15,3)
+# blur = cv2.blur(img,(7,7))
+# cv2.imwrite("open_cv.png", blur)
+#=================================================================
 
 # call histogram
 # img = cv2.cvtColor(img , cv2.COLOR_BGR2GRAY)
@@ -27,12 +24,20 @@ normalization.normalize(img ,new_max ,new_min)
 # call equaliztion
 
 img = cv2.cvtColor(img , cv2.COLOR_BGR2GRAY)
-img = cv2.convertScaleAbs(img ,alpha= 1.10 ,beta= -20)
 equalization.histogram_equaliztion(img)
 equaled_hist = equalization.Histogram (img)
-filters.average_filter(img, 3)
-filters.median_filter(img)
-filters.gaussian_filter(img,15,3)
-blur = cv2.blur(img,(7,7))
-cv2.imwrite("open_cv.png", blur)
+plt.show()
 # =====================================================
+
+#call normalization
+# new_max = 255 
+# new_min = 100
+# normalization.normalize(img ,new_max ,new_min)
+#=========================================================
+
+# show original image
+
+cv2.imshow('image',img) # show original image
+cv2.waitKey(0)
+#=========================================================
+
