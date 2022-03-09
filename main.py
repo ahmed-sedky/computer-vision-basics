84db1dbf26d96a4009d1a5801e6683db9d87a956
Mo'men Maged <momen99223@gmail.com>
2022-03-09 19:44:43 +0200

    added notebook

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ import matplotlib.pyplot as plt
 from PIL import Image
 
 # Load an color image
-# img = Image.open("images/img2.png")
+img = cv2.imread("images/lenna.png")
 # ========================================================
 
 # show original image3
 # call filters
-# img = cv2.cvtColor(img , cv2.COLOR_BGR2GRAY)
+img = cv2.cvtColor(img , cv2.COLOR_BGR2GRAY)
 # filters.average_filter(img, 3)
 # filters.median_filter(img)
-# filters.gaussian_filter(img,15,3)
+filters.gaussian_filter(img,15,3)
 # blur = cv2.blur(img,(7,7))
 # cv2.imwrite("open_cv.png", blur)
 # =================================================================
@@ -26,7 +26,6 @@ from PIL import Image
 
 # img = cv2.cvtColor(img , cv2.COLOR_BGR2GRAY)
 # img = img.convert('L')
-img = cv2.imread('images/lenna.png') 
 # img = cv2.cvtColor(img , cv2.COLOR_BGR2GRAY)
 equalization.equalize(img)
 equaled_hist = equalization.Histogram (img)
