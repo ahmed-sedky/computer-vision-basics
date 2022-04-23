f5c45633dc607a740def070ce3abb77ae4cb6d04
Sedky99 <hossedky@gmail.com>
2022-04-23 23:43:39 +0200

    Use cv2.sobel & cv2.gaussian

--- libs/harris.py
+++ libs/harris.py
@@ -3,11 +3,14 @@ import cv2
 import matplotlib.pyplot as plt
 from libs import filters,Sobel
 def harrisCorner(image ,k):
-    grayscale_image = filters.grayscale(image)
-    Ix,Iy = Sobel.sobel(image,True)
-    Ixx = filters.gaussian_filter(Ix**2, size = 5, std_dev=  1)
-    Iyy = filters.gaussian_filter(Iy**2, size = 5, std_dev=  1)
-    Ixy = filters.gaussian_filter(Ix*Iy, size = 5, std_dev=  1)
+    src = np.copy(image)
+    src = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+
+    Ix = cv2.Sobel(src, cv2.CV_64F, 1, 0, ksize=5)
+    Iy = cv2.Sobel(src, cv2.CV_64F, 0, 1, ksize=5)
+    Ixx = cv2.GaussianBlur(src=Ix ** 2, ksize=(5, 5), sigmaX=0)
+    Ixy = cv2.GaussianBlur(src=Iy * Ix, ksize=(5, 5), sigmaX=0)
+    Iyy = cv2.GaussianBlur(src=Iy ** 2, ksize=(5, 5), sigmaX=0)
 
     det = Ixx *Iyy - (Ixy**2)
     trace = Ixx + Iyy
@@ -20,7 +23,7 @@ def corner2Image (image, harrisResponse , cornerThreshold = 0.01 ):
     harrisMatrix =cv2.dilate(cop_harris,None)
     hMax  = harrisMatrix.max()
     corner_indices = np.array(harrisMatrix > (hMax * cornerThreshold), dtype="int8")
-    corner_indices = corner_indices[2:,2:]
+    # corner_indices = corner_indices[2:,2:]
     image[corner_indices == 1 ] = [0 , 255 ,0]
     return image    
 
