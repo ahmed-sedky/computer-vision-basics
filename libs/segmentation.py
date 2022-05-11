d46e4916aed267cad97beb62765a2b37456b2f4b
Sedky99 <hossedky@gmail.com>
2022-05-12 00:25:10 +0200

    Add region growing algo

--- libs/segmentation.py
+++ libs/segmentation.py
@@ -2,6 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 from libs import colorMap
+
+class Point(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+
 # KMeans Algorithm
 def euclidean_distance(x1, x2):
     return np.sqrt(np.sum((x1 - x2) ** 2))
@@ -120,6 +132,52 @@ class KMeans:
     def cent(self):
         return self.centroids
 
+def getGrayDiff(img, currentPoint, tmpPoint):
+    return abs(int(img[currentPoint.x, currentPoint.y]) - int(img[tmpPoint.x, tmpPoint.y]))
+
+
+def selectConnects(p):
+    if p != 0:
+        connects = [Point(-1, -1), Point(0, -1), Point(1, -1),
+                    Point(1, 0), Point(1, 1), Point(0, 1),
+                    Point(-1, 1), Point(-1, 0)]
+    else:
+        connects = [Point(0, -1), Point(1, 0), Point(0, 1), Point(-1, 0)]
+
+    return connects
+
+def regionGrow(img, seeds, thresh, p = 1):
+    height, weight = img.shape
+    seedMark = np.zeros(img.shape)
+    seedList = []
+
+    for seed in seeds:
+        seedList.append(seed)
+    label = 1
+    connects = selectConnects(p)
+
+    while (len(seedList) > 0):
+        currentPoint = seedList.pop(0)
+
+        seedMark[currentPoint.x, currentPoint.y] = label
+
+        for i in range(8):
+            tmpX = currentPoint.x + connects[i].x
+            tmpY = currentPoint.y + connects[i].y
+
+            if tmpX < 0 or tmpY < 0 or tmpX >= height or tmpY >= weight:
+                continue
+
+            grayDiff = getGrayDiff(img, currentPoint, Point(tmpX, tmpY))
+
+            if grayDiff < thresh and seedMark[tmpX, tmpY] == 0:
+                seedMark[tmpX, tmpY] = label
+                seedList.append(Point(tmpX, tmpY))
+
+    return seedMark
+
+
+
 def apply_k_means(source, k=5, max_iter=100):
     """Segment image using K-means
     Args:
@@ -150,3 +208,19 @@ def apply_k_means(source, k=5, max_iter=100):
     segmented_image = segmented_image.reshape(source.shape)
 
     return segmented_image, labels
+
+def apply_region_growing(source: np.ndarray):
+
+    src = np.copy(source)
+    color_img = cv2.cvtColor(src, cv2.COLOR_Luv2BGR)
+    img_gray = cv2.cvtColor(color_img, cv2.COLOR_BGR2GRAY)
+    seeds = []
+    for i in range(3):
+        x = np.random.randint(0, img_gray.shape[0])
+        y = np.random.randint(0, img_gray.shape[1])
+        seeds.append(Point(x, y))
+
+    # seeds = [Point(10, 10), Point(82, 150), Point(20, 300)]
+    output_image = regionGrow(img_gray, seeds, 10)
+
+    return output_image
